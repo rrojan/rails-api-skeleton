d6e777f6f4cde2e706c30a2c8a572e7e364da637
Rojan Rajbhandari <rrojan20@gmail.com>
2022-10-08 20:32:51 +0545

    fix: fix jwt payload parsing error

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -20,8 +20,11 @@ class HealthCheckController < ApplicationController
   private
 
   def user_from_token
-    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').first, ENV['DEVISE_JWT_SECRET_KEY'])
+    jwt_payload = JWT.decode(
+      request.headers['Authorization'].split(' ').last,
+      ENV['DEVISE_JWT_SECRET_KEY']
+    ).first
     user_id = jwt_payload['sub']
-    User.find(user_id.to_s)
+    User.find(user_id)
   end
 end
